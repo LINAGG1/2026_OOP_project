6dadd2ff755efbe5308eb67d0a5192bf752bff90
ahyoon7 <cay77@sookmyung.ac.kr>
2026-06-03 19:19:13 +0900

    Update LoginController.java

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,16 +2,19 @@ package controller;
 
 import model.BookRepository;
 import model.User;
+/*
+로그인 관련 요청을 처리하는 Controller
+View에서 전달받은 로그인 정보를 Repository에 전달
+ */
+public class LoginController { 
 
-public class LoginController {
+    private BookRepository repository; // 도서 및 사용자 데이터를 관리하는 Repository
 
-    private BookRepository repository;
-
-    public LoginController(BookRepository repository) {
+    public LoginController(BookRepository repository) { //Repository 객체를 전달받아 Controller와 연결
         this.repository = repository;
     }
-
-    public User login(String userId, String password) {
+ 
+    public User login(String userId, String password) { //로그인 요청 처리
         return repository.login(userId, password);
     }
 }
