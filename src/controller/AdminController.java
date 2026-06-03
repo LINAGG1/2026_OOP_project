f6705b0920d5f0292ca21600deb6aef9700e6047
ahyoon7 <cay77@sookmyung.ac.kr>
2026-06-03 19:28:40 +0900

    Update AdminController.java

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -2,20 +2,23 @@ package controller;
 
 import model.Book;
 import model.BookRepository;
-
+/*
+관리자 전용 기능을 처리하는 Controller
+도서 추가 및 삭제 기능을 담당
+*/
 public class AdminController {
 
-    private BookRepository repository;
+    private BookRepository repository; //도서 데이터를 관리하는 Repository
 
-    public AdminController(BookRepository repository) {
+    public AdminController(BookRepository repository) { //Repository 객체를 전달받아 연결
         this.repository = repository;
     }
 
-    public void addBook(Book book) {
+    public void addBook(Book book) { //새로운 도서를 등록
         repository.addBook(book);
     }
 
-    public void removeBook(String bookId) {
+    public void removeBook(String bookId) { //도서 ID를 이용해 도서를 삭제
         repository.removeBook(bookId);
     }
 }
