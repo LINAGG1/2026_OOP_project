29b8521f283132dae0640c73f9c07d18ada2cd40
su2in <suin9j@gmail.com>
2026-06-05 21:15:49 +0900

    수정

--- src/Main.java
+++ src/Main.java
@@ -36,7 +36,7 @@ public class Main {
 
             if (success) {
                 // 회원가입 성공 시 나경이가 구현한 파일 저장 메소드 호출
-                // repository.saveToFile(); // 나경이가 만든 저장 메소드명에 맞춰 주석 해제해서 사용해!
+                // 메소드명에 맞춰 주석 해제해서 사용해!
             } else {
                 JOptionPane.showMessageDialog(
                         loginView,
