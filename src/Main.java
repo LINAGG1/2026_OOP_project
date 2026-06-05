bcb3650c585bd3de589442e41637a42f7da6cbc5
LINAGG1 <leeng060124@gmail.com>
2026-06-05 17:49:57 +0900

    fix: 메인 파일 수정

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import controller.AdminController;
+import controller.LibraryController;
 import controller.LoginController;
 import model.Book;
 import model.BookRepository;
@@ -18,16 +19,16 @@ public class Main {
 
         // 테스트용 도서
         repository.addBook(
-          new Book("001", "자바 프로그래밍", "홍길동"));
+                new Book("001", "자바 프로그래밍", "홍길동"));
 
         repository.addBook(
-          new Book("002", "자료구조", "김철수"));
+                new Book("002", "자료구조", "김철수"));
 
         repository.addBook(
-          new Book("003", "운영체제", "이영희"));
+                new Book("003", "운영체제", "이영희"));
 
         repository.addBook(
-         new Book("004", "데이터베이스", "박민수"));
+                new Book("004", "데이터베이스", "박민수"));
 
         // 테스트용 계정
         repository.registerUser(
@@ -38,6 +39,7 @@ public class Main {
 
         // 로그인 관련 객체 생성
         LoginView loginView = new LoginView();
+
         LoginController loginController =
                 new LoginController(repository);
 
@@ -74,8 +76,23 @@ public class Main {
             if (user.isAdmin()) {
 
                 AdminView adminView = new AdminView();
+
                 AdminController adminController =
                         new AdminController(repository);
+
+                // 기존 도서 목록 표시
+                for (Book book : repository.getBooks()) {
+
+                    adminView.getTableModel().addRow(
+                            new Object[]{
+                                    book.getBookId(),
+                                    book.getTitle(),
+                                    book.getAuthor(),
+                                    book.isBorrowed()
+                                            ? "대여중"
+                                            : "대여가능"
+                            });
+                }
 
                 // 도서 등록
                 adminView.addInsertListener(ev -> {
@@ -99,7 +116,7 @@ public class Main {
                                     book.getBookId(),
                                     book.getTitle(),
                                     book.getAuthor(),
-                                    "대여 가능"
+                                    "대여가능"
                             });
 
                     adminView.clearInputFields();
@@ -158,6 +175,145 @@ public class Main {
 
                 BookSearchView searchView =
                         new BookSearchView();
+
+                LibraryController libraryController =
+                        new LibraryController(repository);
+
+                // 처음 실행 시 전체 도서 표시
+                for (Book book : repository.getBooks()) {
+
+                    searchView.getTableModel().addRow(
+                            new Object[]{
+                                    book.getBookId(),
+                                    book.getTitle(),
+                                    book.getAuthor(),
+                                    book.isBorrowed()
+                                            ? "대여중"
+                                            : "대여가능"
+                            });
+                }
+
+                // 검색 버튼
+                searchView.addSearchListener(ev -> {
+
+                    String category =
+                            searchView.getSearchCategory();
+
+                    String keyword =
+                            searchView.getSearchKeyword();
+
+                    searchView.getTableModel().setRowCount(0);
+
+                    if (keyword.isEmpty()) {
+                      for (Book book : repository.getBooks()) {
+                        searchView.getTableModel().addRow(
+                            new Object[]{
+                                    book.getBookId(),
+                                    book.getTitle(),
+                                    book.getAuthor(),
+                                    book.isBorrowed()
+                                            ? "대여중"
+                                            : "대여가능"
+                            });
+                      }
+                      return;
+                    }
+
+                    if (category.equals("도서명")) {
+
+                        for (Book book :
+                                libraryController.searchBookByTitle(keyword)) {
+
+                            searchView.getTableModel().addRow(
+                                    new Object[]{
+                                            book.getBookId(),
+                                            book.getTitle(),
+                                            book.getAuthor(),
+                                            book.isBorrowed()
+                                                    ? "대여중"
+                                                    : "대여가능"
+                                    });
+                        }
+                    }
+
+                    else if (category.equals("저자")) {
+
+                        for (Book book :
+                                libraryController.searchBookByAuthor(keyword)) {
+
+                            searchView.getTableModel().addRow(
+                                    new Object[]{
+                                            book.getBookId(),
+                                            book.getTitle(),
+                                            book.getAuthor(),
+                                            book.isBorrowed()
+                                                    ? "대여중"
+                                                    : "대여가능"
+                                    });
+                        }
+                    }
+
+                    else if (category.equals("도서 ID")) {
+
+                        Book book =
+                                libraryController.searchBookByBookId(keyword);
+
+                        if (book != null) {
+
+                            searchView.getTableModel().addRow(
+                                    new Object[]{
+                                            book.getBookId(),
+                                            book.getTitle(),
+                                            book.getAuthor(),
+                                            book.isBorrowed()
+                                                    ? "대여중"
+                                                    : "대여가능"
+                                    });
+                        }
+                    }
+                });
+
+                // 대여 버튼
+                searchView.addRentListener(ev -> {
+
+                    String bookId =
+                            searchView.getSelectedBookId();
+
+                    boolean success =
+                            libraryController.borrowBook(
+                                    user.getUserId(),
+                                    bookId
+                            );
+
+                    if (success) {
+
+                      searchView.getTableModel().setRowCount(0);
+                      for (Book book : repository.getBooks()) {
+                        searchView.getTableModel().addRow(
+                            new Object[]{
+                                    book.getBookId(),
+                                    book.getTitle(),
+                                    book.getAuthor(),
+                                    book.isBorrowed()
+                                            ? "대여중"
+                                            : "대여가능"
+                            });
+                      }
+
+                      JOptionPane.showMessageDialog(
+                              searchView,
+                              "대여가 완료되었습니다."
+                      );
+                      
+
+                    } else {
+
+                        JOptionPane.showMessageDialog(
+                                searchView,
+                                "대여할 수 없습니다."
+                        );
+                    }
+                });
 
                 searchView.setVisible(true);
             }
